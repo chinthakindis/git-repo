4ae7f26b168f3ab6e46b798fe374bb9ddf420325
chinthakindis <38332362+chinthakindis@users.noreply.github.com>
2018-04-20 15:08:57 -0400

    added Hello World line

--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -10,6 +6,7 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
